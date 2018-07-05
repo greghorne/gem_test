6d9418ae1367afc7419042a07367b2f2c1d3af73
Greg Horne <greghorne@hotmail.com>
2018-07-05 18:41:16 +0000

    updated code; tracing error

--- lib/gem1.rb
+++ lib/gem1.rb
@@ -3,16 +3,16 @@ require "pg"
 
 module PG_Utilities
   class PG_Connection
-    def self.connect(host, dbname, username, password, port)
-      begin
-        PG::Connection.open(:host => host, :dbname => dbname, :username => username, :password => password, :port => port)
-      rescue
-        return PG::Error
-      end
-    end
+    # def self.connect(host, dbname, username, password, port)?
+    #   begin
+    #     PG::Connection.open(:host => host, :dbname => dbname, :username => username, :password => password, :port => port)
+    #   rescue
+    #     return PG::Error
+    #   end
+    # end
 
-    def self.msg(message)
-      return "you said: " + message.to_s
+    def self.msg?
+      return "you said: " #+ message.to_s
     end
   end
 end
