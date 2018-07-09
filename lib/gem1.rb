f9c74b50c85cbe4c3e51543680c7d839bba6983a
Greg Horne <greghorne@hotmail.com>
2018-07-09 21:41:08 +0000

    updated code; cleanup

--- lib/gem1.rb
+++ lib/gem1.rb
@@ -29,10 +29,6 @@ module PGPack
       # require sslmode
       db_hash[:sslmode] || db_hash[:sslmode] = "require"
 
-      # encrypted = PG::Connection.encrypt_password(db_hash[:user], db_hash[:password])
-      # puts encrypted
-      # db_hash[:password] = encrypted
-
       begin
         conn = PG::Connection.open(db_hash)
 
