bacf79ec47b4e9c4795dcaa096d3f16d53078d0e
Greg Horne <greghorne@hotmail.com>
2018-07-09 17:06:32 +0000

    updated test

--- test/test.rb
+++ test/test.rb
@@ -4,27 +4,43 @@ require "pg"
 repeat = 20
 sum = 0
 
-repeat.times {
+def test_gem(db_hash, repeat)
+    sum = 0
+    repeat.times {
 
-    db_hash = { :host => ENV[PG_HOST],
-                :dbname => ENV[PG_DB],
-                :user => ENV[PG_USER],
-                :password => ENV[PG_PASSWORD],
-                :port => 2346,
-                :hostaddr => ENV[PG_HOSTADDR],
-                :sslmode => "require"
+        db_hash = { :host => ENV["PG_HOST"],
+                    :dbname => ENV["PG_DB"],
+                    :user => ENV["PG_USER"],
+                    :password => ENV["PG_PASSWORD"],
+                    :port => ENV["PG_PORT"],
+                    :hostaddr => ENV["PG_HOSTADDR"],
+                    :sslmode => ENV["PG_SSLMODE"]
+        }
+
+        start = Time.now
+
+        response = PGPack::PGConnect.connect(db_hash)
+
+        conn = response[:conn]
+        response_query = conn.query("select count(*) from world")
+
+        PGPack::PGConnect.disconnect(conn)
+
+        sum += (Time.now - start)
     }
 
-    start = Time.now
+    puts sum / repeat 
+end
 
-    response = PGPack::PGConnect.connect(db_hash)
+test_gem({  :host => ENV["PG_HOST"],
+            :dbname => ENV["PG_DB"],
+            :user => ENV["PG_USER"],
+            :password => ENV["PG_PASSWORD"],
+            :port => ENV["PG_PORT"],
+            :hostaddr => ENV["PG_HOSTADDR"],
+            :sslmode => ENV["PG_SSLMODE"]
+        }, 20
+)
 
-    conn = response[:conn]
-    response_query = conn.query("select count(*) from world")
 
-    PGPack::PGConnect.disconnect(conn)
 
-    sum += (Time.now - start)
-}
-
-puts sum / repeat 
